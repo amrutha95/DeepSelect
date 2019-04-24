4f9ef74472c266fa46f016869f123cb733d05597
amrutha95 <amrutha.srinivasan95@gmail.com>
2019-04-23 18:24:34 -0700

    Update loader.py

--- loader.py
+++ loader.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+from torch.utils.data import sampler
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms

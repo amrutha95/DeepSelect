dad88275d63ef7003a38f5c19bc8bd84b8a7334e
Prateek Malhotra <prateekdbst@gmail.com>
2019-05-01 12:35:46 -0700

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -9,8 +9,10 @@ def visualise(middle, predicted_class, neurons_per_class=100):
   middle_np = torch.abs(middle).data.cpu().numpy().squeeze()
   plt.figure()
   plt.plot(middle_np)
+  plt.title("Activation pattern for the last hidden layer")
   ones = np.zeros((10 * neurons_per_class))
   ones[predicted_class * neurons_per_class:(predicted_class + 1)* neurons_per_class] = 1.0
   
   plt.figure()
   plt.plot(ones)
+  plt.title("Template")

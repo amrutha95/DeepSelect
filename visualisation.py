1ad2de844b02e94f2e3eba4f242de87a813fcfcf
amrutha95 <amrutha.srinivasan95@gmail.com>
2019-04-30 17:41:47 -0700

    Create visualisation.py

---
+++ visualisation.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import torch
+def visualise(middle, predicted_class):
+  middle_np = torch.abs(middle).data.cpu().numpy().squeeze()
+  plt.figure()
+  plt.plot(middle_np)
+  ones = np.zeros((1000))
+  ones[predicted * 100:(predicted + 1)* 100] = 1.0
+  plt.figure()
+  plt.plot(ones)

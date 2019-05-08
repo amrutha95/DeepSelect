382ce5d4dbff77068865f4bcc63f8b7588b74b26
amrutha95 <amrutha.srinivasan95@gmail.com>
2019-05-07 18:32:32 -0700

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -7,17 +7,32 @@ import numpy as np
 
 class_mapping = {0:"Airplane", 1:"Automobile", 2:"Bird", 3:"Cat", 4:"Deer", 5:"Dog", 6:"Frog", 7:"Horse", 8:"Ship", 9:"Truck"}
 
-def visualise(x, middle, predicted_class, neurons_per_class=100):
+def visualise(x, middle, neurons_per_class=100):
   x = x.squeeze().data.cpu().numpy().transpose(1, 2, 0)
   plt.imshow(x)
-  plt.title("Class: {} [{}]".format(class_mapping[predicted_class], predicted_class))
+  #plt.title("Class: {}".format(clapredicted_class))
     
   middle_np = torch.abs(middle).data.cpu().numpy().squeeze()
   plt.figure()
   plt.plot(middle_np)
   plt.title("Activation pattern for the last hidden layer")
+  min_kl = 0
+  kl_div = 0
+  min_class = 0
+  middle_layer = torch.log(middle + 0.01).type(dtype)
+  for i in np.arange(0,10):
+    ones = np.zeros((10 * neurons_per_class))
+    ones[i * neurons_per_class:(i + 1)* neurons_per_class] = 1.0
+    kl_div = F.kl_div(middle_layer, template, reduction='sum').sum()
+    if i == 0:
+      min_kl = kl_div
+    else if kl_div < min_kl:
+      min_kl = kl_div
+      min_class = i
+     
+    
   ones = np.zeros((10 * neurons_per_class))
-  ones[predicted_class * neurons_per_class:(predicted_class + 1)* neurons_per_class] = 1.0
+  ones[i * neurons_per_class:(i + 1)* neurons_per_class] = 1.0
   
   plt.figure()
   plt.plot(ones)

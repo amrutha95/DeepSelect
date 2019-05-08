46918e6bed8b88844a9340a0bd045a966e8a9750
amrutha95 <amrutha.srinivasan95@gmail.com>
2019-05-07 17:18:59 -0700

    Added modified test for only KL div loss

--- methods.py
+++ methods.py
@@ -38,13 +38,13 @@ def train(model, loss_fn, optimizer, epochs, loaders, tuning=0.1, neurons_per_cl
         template[indexes] = 1.0
         
         middle_layer = torch.log(middle + 0.01).type(dtype)
-        loss1 = loss_fn(preds,y)                                          #Default = CrossEntropyLoss
+        #loss1 = loss_fn(preds,y)                                          #Default = CrossEntropyLoss
         loss2 = F.kl_div(middle_layer, template, reduction='sum').sum()
-        epoch_loss_acc += loss1.data.item()
+        #epoch_loss_acc += loss1.data.item()
         epoch_loss_kl += loss2.data.item()
         
-        loss = loss1 + tuning * loss2
-        
+        #loss = loss1 + tuning * loss2
+        loss = loss2
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -52,7 +52,7 @@ def train(model, loss_fn, optimizer, epochs, loaders, tuning=0.1, neurons_per_cl
     train_acc = test(model, train_loader)
     print("Training accuracy for epoch {} is {}".format(i + 1, train_acc))
     
-    print("Training loss (Cross Entropy) is {}".format(epoch_loss_acc))
+    #print("Training loss (Cross Entropy) is {}".format(epoch_loss_acc))
     print("Training loss (KL divergence) is {}".format(epoch_loss_kl))
     
     if test_mode == False:
@@ -76,3 +76,20 @@ def test(model, loader):
           correct += (predicted == labels).sum().item()
 
   return 100 * correct / total
+
+  def test_KL(model, loader, neurons_per_class):
+    kl_div_sum = 0 
+    total = 0
+    model.eval()
+    with torch.no_grad():
+      for data in loader:
+        images, labels = data
+        images = Variable(images).type(dtype)
+        class_number = Variable(labels).type(long_dtype)
+        indexes = torch.arange(class_number * neurons_per_class, (class_number + 1) * neurons_per_class)
+        template = torch.zeros((10 * neurons_per_class)).type(dtype)      #CIFAR-10 specific
+        template[indexes] = 1.0
+        middle = model(images)         
+        kl_div_sum += F.kl_div(middle, template, reduction='sum').sum()
+        total += label.size(0)
+  return kl_div_sum/total
